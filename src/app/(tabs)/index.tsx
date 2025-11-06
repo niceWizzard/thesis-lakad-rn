536db18d0bcfb515e995ac879b0813f81a8a298b
niceWizzard <richard232manansala@gmail.com>
2025-11-06 18:32:53 +0800

    Add historical landmarks markers and improve map interaction in ExploreTab

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,14 +1,98 @@
 import { Pressable, StyleSheet } from 'react-native';
 
 import ExploreSearchBox from '@/src/components/ExploreSearchBox';
-import { View } from '@/src/components/Themed';
+import { Text, View } from '@/src/components/Themed';
 import { Ionicons } from '@expo/vector-icons';
-import { Camera, Location, LocationPuck, MapView, UserLocation } from '@rnmapbox/maps';
+import { Camera, Location, LocationPuck, MapView, MarkerView, UserLocation } from '@rnmapbox/maps';
 import { useRef, useState } from 'react';
 
 // Define the default coordinates
 const DEFAULT_COORDS: [number, number] = [120.8092, 14.8605];
 const TARGET_ZOOM = 15;
+
+const historicalLandmarks = [
+  {
+    name: "Barasoain Church",
+    id: 1,
+    city: "CITY OF MALOLOS",
+    categoryMain: "HISTORY AND CULTURE",
+    categorySub: "Church",
+    latitude: 14.84646279,
+    longitude: 120.8126497,
+  },
+  {
+    name: "Basilica Minore de Immaculada Concepcion",
+    id: 2,
+    city: "CITY OF MALOLOS",
+    categoryMain: "HISTORY AND CULTURE",
+    categorySub: "Church",
+    latitude: 14.84298357,
+    longitude: 120.8115019,
+  },
+  {
+    name: "Bautista Mansion",
+    id: 3,
+    city: "CITY OF MALOLOS",
+    categoryMain: "HISTORY AND CULTURE",
+    categorySub: "Landmark",
+    latitude: 14.84270054,
+    longitude: 120.8104554,
+  },
+  {
+    name: "Cojuangco House",
+    id: 4,
+    city: "CITY OF MALOLOS",
+    categoryMain: "HISTORY AND CULTURE",
+    categorySub: "Landmark",
+    latitude: 14.84500138,
+    longitude: 120.8126748,
+  },
+  {
+    name: "Gen. Isodoro Torres Marker",
+    id: 5,
+    city: "CITY OF MALOLOS",
+    categoryMain: "HISTORY AND CULTURE",
+    categorySub: "Landmark",
+    latitude: 14.84301953,
+    longitude: 120.8121215,
+  },
+  {
+    name: "Instituto de Mujeres",
+    id: 6,
+    city: "CITY OF MALOLOS",
+    categoryMain: "HISTORY AND CULTURE",
+    categorySub: "Landmark",
+    latitude: 14.8426084,
+    longitude: 120.8096216,
+  },
+  {
+    name: "Kamestisuhan/ Calle Paranchillo",
+    id: 7,
+    city: "CITY OF MALOLOS",
+    categoryMain: "HISTORY AND CULTURE",
+    categorySub: "Landmark",
+    latitude: 14.841805,
+    longitude: 120.8105982,
+  },
+  {
+    name: "Siar Tree",
+    id: 8,
+    city: "CITY OF MALOLOS",
+    categoryMain: "HISTORY AND CULTURE",
+    categorySub: "Landmark",
+    latitude: 14.84289295,
+    longitude: 120.8116524,
+  },
+  {
+    name: "Reyes House",
+    id: 9,
+    city: "CITY OF MALOLOS",
+    categoryMain: "HISTORY AND CULTURE",
+    categorySub: "Landmark",
+    latitude: 14.8422209,
+    longitude: 120.8102771,
+  },
+];
 
 
 const ExploreTab = () => {
@@ -16,20 +100,16 @@ const ExploreTab = () => {
     const camera = useRef<Camera>(null);
     const map = useRef<MapView>(null);
     const [userLocation, setUserLocation] = useState<Location>()
+    const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
 
 
     // Function to handle the "locate" button press
-    const centerMapOnUser = () => {
-        // Get the user's coordinates, falling back to the default if not available
-        const coordinates: [number, number] = userLocation?.coords.longitude && userLocation?.coords.latitude
-            ? [userLocation.coords.longitude, userLocation.coords.latitude]
-            : DEFAULT_COORDS;
-
+    const centerMapOnCoord = (coordinates: [number, number], zoom?: number) => {
         // Use the camera ref and the flyTo method to update position and zoom with an animation
         if (camera.current) {
             camera.current?.setCamera({
                 centerCoordinate: coordinates,
-                zoomLevel: TARGET_ZOOM,
+                zoomLevel: zoom ?? TARGET_ZOOM,
                 animationDuration: 500, 
             })
         } else {
@@ -62,11 +142,58 @@ const ExploreTab = () => {
             <LocationPuck 
               visible
             />
+
+            {
+              historicalLandmarks.map((landmark, index) => (
+                <MarkerView
+                  key={`landmark-${landmark.id}`}
+                  coordinate={[landmark.longitude, landmark.latitude]}
+                  >
+                  <View style={{alignItems: 'center', backgroundColor: 'transparent',}}>
+                    <Pressable
+                      onPress={() =>
+                        {
+                          setSelectedIndex(index);
+                          centerMapOnCoord([landmark.longitude, landmark.latitude], 18);
+                        }
+                      }
+                    >
+                      <Ionicons 
+                          name='location-sharp'
+                          size={32}
+                          color='red'
+                        />
+                    </Pressable>
+                    <Text>{landmark.name}</Text>
+                  </View>
+                </MarkerView>
+              ))
+            }
           </MapView>
+          {
+            selectedIndex !== null && (
+              <View style={{position: 'absolute', bottom: 0, left: 0, right: 0, height: '20%', padding: 12, zIndex: 10}} >
+                <Pressable>
+                  <Ionicons 
+                    name='close-circle'
+                    size={32}
+                    style={{alignSelf: 'flex-end'}}
+                    onPress={() => setSelectedIndex(null)}
+                  />
+                </Pressable>
+                <Text>{historicalLandmarks[selectedIndex].name}</Text>
+              </View>
+            )
+          }
           <ExploreSearchBox />
           {/* Update the onPress handler to use the new function */}
           <Pressable 
-          onPress={centerMapOnUser}
+            onPress={() => {
+              const coordinates: [number, number] = userLocation?.coords.longitude && userLocation?.coords.latitude
+              ? [userLocation.coords.longitude, userLocation.coords.latitude]
+              : DEFAULT_COORDS;
+              centerMapOnCoord(coordinates);
+            }}
           style={{
              position: 'absolute', bottom: 32, right: 16, backgroundColor: 'white', padding: 12, borderRadius: 8
             }}>
