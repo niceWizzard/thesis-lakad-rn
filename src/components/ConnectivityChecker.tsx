027d20e4ea9af4ab0510bcfd90cc51d90bdb3b8b
niceWizzard <richard232manansala@gmail.com>
2026-01-08 20:36:24 +0800

    Improved connectivity checker - fixed header being too cramped due to returning null

--- src/components/ConnectivityChecker.tsx
+++ src/components/ConnectivityChecker.tsx
@@ -1,47 +1,43 @@
 import { Text } from '@/components/ui/text';
 import * as Network from 'expo-network';
 import React, { useEffect, useState } from 'react';
-import { SafeAreaView } from 'react-native-safe-area-context';
-const ConnectivityChecker = () => {
+import { View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
-    const [bgColor, setBgColor] = useState('gray')
+const ConnectivityChecker = () => {
     const [networkState, setNetworkState] = useState<Network.NetworkState | null>(null);
+    const insets = useSafeAreaInsets();
 
     useEffect(() => {
-        // Fetch initial state
-        const fetchInitialState = async () => {
-            const state = await Network.getNetworkStateAsync();
-            setNetworkState(state);
-        };
-        fetchInitialState();
-
-        // Subscribe to network changes
-        const networkListener = Network.addNetworkStateListener((state) => {
-            setNetworkState(state);
-        });
-
-        // Cleanup: Remove listener when component unmounts
-        return () => {
-            networkListener.remove();
-        };
+        Network.getNetworkStateAsync().then(setNetworkState);
+        const networkListener = Network.addNetworkStateListener(setNetworkState);
+        return () => networkListener.remove();
     }, []);
 
-    if (networkState?.isInternetReachable) {
-        return null;
-    }
+    // If internet is reachable, we don't want to render anything (0 height)
+    if (networkState?.isInternetReachable !== false)
+        return <View style={{
+            paddingTop: insets.top,
+        }} className='bg-background-0'>
+
+        </View>;
 
     return (
-        <SafeAreaView
-            edges={['top', 'left', 'right']}
+        <View
             style={{
-                backgroundColor: bgColor,
+                paddingTop: insets.top,
                 paddingBottom: 8,
+                width: '100%',
                 justifyContent: 'center',
                 alignItems: 'center',
-            }}>
-            <Text >No internet connection.</Text>
-        </SafeAreaView>
-    )
+            }}
+            className='bg-error-800'
+        >
+            <Text className="text-white font-bold text-xs">
+                No internet connection.
+            </Text>
+        </View>
+    );
 }
 
-export default ConnectivityChecker
+export default ConnectivityChecker;
