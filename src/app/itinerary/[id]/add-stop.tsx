38d7c0546770837c0b5033585b82edacf7e2f9fa
niceWizzard <richard232manansala@gmail.com>
2026-01-25 22:03:14 +0800

    Add ability to add manual stops

--- src/app/itinerary/[id]/add-stop.tsx
+++ src/app/itinerary/[id]/add-stop.tsx
@@ -2,12 +2,12 @@ import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { useLocalSearchParams, useRouter } from 'expo-router';
 import debounce from 'lodash.debounce';
 import { Map as MapIcon, MapPin, Plus, Search, X } from 'lucide-react-native';
-import React, { useMemo, useState } from 'react';
-import { ActivityIndicator, FlatList, Image, Pressable } from 'react-native';
+import React, { useMemo, useRef, useState } from 'react';
+import { ActivityIndicator, FlatList, Image, Pressable, View } from 'react-native';
 
 // UI Components
 import { Box } from '@/components/ui/box';
-import { Button, ButtonIcon, ButtonSpinner } from '@/components/ui/button';
+import { Button, ButtonIcon, ButtonSpinner, ButtonText } from '@/components/ui/button';
 import { Heading } from '@/components/ui/heading';
 import { HStack } from '@/components/ui/hstack';
 import { Icon } from '@/components/ui/icon';
@@ -16,9 +16,12 @@ import { Text } from '@/components/ui/text';
 import { VStack } from '@/components/ui/vstack';
 
 // Logic & Utils
+import { AlertDialog, AlertDialogBackdrop, AlertDialogBody, AlertDialogContent, AlertDialogFooter, AlertDialogHeader } from '@/components/ui/alert-dialog';
+import CustomMapView from '@/src/components/CustomMapView';
 import { useToastNotification } from '@/src/hooks/useToastNotification';
-import { insertLandmarkToItinerary } from '@/src/utils/insertLandmark';
+import { createLandmark, insertLandmarkToItinerary } from '@/src/utils/insertLandmark';
 import { supabase } from '@/src/utils/supabase';
+import Mapbox, { PointAnnotation } from '@rnmapbox/maps';
 
 export default function AddPOIScreen() {
     const { id: itineraryId, currentCount } = useLocalSearchParams();
@@ -26,9 +29,10 @@ export default function AddPOIScreen() {
     const { showToast } = useToastNotification();
     const queryClient = useQueryClient();
 
+    const [showManualDialog, setShowManualDialog] = useState(false)
+
     const [searchQuery, setSearchQuery] = useState('');
 
-    // 1. Debounced Search Logic
     const debouncedSearch = useMemo(
         () => debounce((text: string) => setSearchQuery(text), 300),
         []
@@ -50,16 +54,16 @@ export default function AddPOIScreen() {
         },
     });
 
-    // 2. Optimized Mutation
     const addStopMutation = useMutation({
         mutationFn: async (landmarkId: number) => insertLandmarkToItinerary({
             currentCount: currentCount.toString(),
             itineraryId: itineraryId.toString(),
             landmarkId: landmarkId.toString(),
         }),
-        onSuccess: () => {
+        onSuccess: async () => {
             // Invalidate queries so the itinerary refreshes when we go back
-            queryClient.invalidateQueries({ queryKey: ['itinerary', itineraryId] });
+            await queryClient.invalidateQueries({ queryKey: ['itinerary', itineraryId] });
+            await queryClient.invalidateQueries({ queryKey: ['itineraries'] });
             showToast({ title: "Added to Itinerary", action: "success" });
             router.back();
         },
@@ -68,103 +72,203 @@ export default function AddPOIScreen() {
         }
     });
 
+    const handleAddManualLocation = async (location: GeoJSON.Position) => {
+        try {
+            const landmarkId = await createLandmark({
+                name: `${location[1]}, ${location[0]}`,
+                longitude: location[0],
+                latitude: location[1],
+                district: "1",
+                municipality: 'Angat',
+                created_by_user: true,
+            });
+            await addStopMutation.mutateAsync(landmarkId)
+
+        } catch (e: any) {
+            showToast({ title: "Error", description: e.message, action: "error" });
+        }
+    }
+
     return (
-        <Box className="flex-1 bg-background-0">
-            <VStack className="p-6 pt-12 flex-1" space="xl">
-                <VStack space="xs">
-                    <Heading size="xl">Add a Stop</Heading>
-                    <Text size="sm" className="text-typography-500">Find landmarks to add to your journey</Text>
-                </VStack>
-
-                <Input variant="outline" size="xl" className="rounded-2xl bg-background-50 border-outline-100">
-                    <InputSlot className="pl-4">
-                        <InputIcon as={Search} className="text-primary-500" />
-                    </InputSlot>
-                    <InputField
-                        placeholder="Search landmarks..."
-                        onChangeText={debouncedSearch}
-                    />
-                    {searchQuery.length > 0 && (
-                        <InputSlot className="pr-4" onPress={() => setSearchQuery('')}>
-                            <Icon as={X} size="sm" />
-                        </InputSlot>
-                    )}
-                </Input>
-
-                {isLoading ? (
-                    <VStack className="flex-1 justify-center items-center" space="md">
-                        <ActivityIndicator size="large" color="#0891b2" />
-                        <Text size="sm" className="text-typography-400">Loading landmarks...</Text>
+        <>
+            <LocationDialogSelection
+                show={showManualDialog}
+                onClose={() => setShowManualDialog(false)}
+                onConfirmLocation={handleAddManualLocation}
+            />
+            <Box className="flex-1 bg-background-0">
+                <VStack className="p-6 pt-12 flex-1" space="xl">
+                    <VStack space="xs">
+                        <Heading size="xl">Add a Stop</Heading>
+                        <Text size="sm" className="text-typography-500">Find landmarks to add to your journey</Text>
                     </VStack>
-                ) : (
-                    <FlatList
-                        data={landmarks}
-                        keyExtractor={(item) => item.id.toString()}
-                        showsVerticalScrollIndicator={false}
-                        ListEmptyComponent={
-                            <VStack className="items-center justify-center py-20" space="md">
-                                <Icon as={MapIcon} size="xl" className="text-outline-200" />
-                                <Text className="text-typography-400">No landmarks found</Text>
-                            </VStack>
-                        }
-                        renderItem={({ item }) => (
-                            <Pressable
-                                className="mb-4 bg-background-0 border border-outline-100 rounded-3xl overflow-hidden active:bg-background-50"
-                                onPress={() => router.navigate({
-                                    pathname: '/landmark/[id]/view',
-                                    params: {
-                                        id: item.id.toString(),
-                                        itineraryId: itineraryId.toString(),
-                                        currentCount: currentCount,
-                                    },
-                                })}
-                            >
-                                <HStack className="items-center p-3" space="md">
-                                    {/* Thumbnail Preview */}
-                                    <Box className="h-16 w-16 bg-background-100 rounded-2xl overflow-hidden">
-                                        {item.image_url ? (
-                                            <Image source={{ uri: item.image_url }} className="w-full h-full" />
-                                        ) : (
-                                            <Box className="items-center justify-center flex-1">
-                                                <Icon as={MapPin} className="text-outline-300" />
-                                            </Box>
-                                        )}
-                                    </Box>
 
-                                    <VStack className="flex-1">
-                                        <Text className="font-bold text-typography-900" numberOfLines={1}>
-                                            {item.name}
-                                        </Text>
-                                        <HStack space="xs" className="items-center">
-                                            <Icon as={MapPin} size="xs" className="text-typography-400" />
-                                            <Text size="xs" className="text-typography-500">
-                                                {item.municipality}
-                                            </Text>
-                                        </HStack>
-                                    </VStack>
-
-                                    <Button
-                                        size="sm"
-                                        action="primary"
-                                        className="rounded-xl h-10 w-10 p-0"
-                                        onPress={(e) => {
-                                            e.stopPropagation();
-                                            return addStopMutation.mutate(item.id);
-                                        }}
-                                        isDisabled={addStopMutation.isPending}
-                                    >
-                                        {addStopMutation.isPending && addStopMutation.variables === item.id ? (
-                                            <ButtonSpinner />
-                                        ) : (
-                                            <ButtonIcon as={Plus} />
-                                        )}
-                                    </Button>
-                                </HStack>
-                            </Pressable>
+                    <Input variant="outline" size="xl" className="rounded-2xl bg-background-50 border-outline-100">
+                        <InputSlot className="pl-4">
+                            <InputIcon as={Search} className="text-primary-500" />
+                        </InputSlot>
+                        <InputField
+                            placeholder="Search landmarks..."
+                            onChangeText={debouncedSearch}
+                        />
+                        {searchQuery.length > 0 && (
+                            <InputSlot className="pr-4" onPress={() => setSearchQuery('')}>
+                                <Icon as={X} size="sm" />
+                            </InputSlot>
                         )}
-                    />
-                )}
-            </VStack>
-        </Box>
+                    </Input>
+
+                    {isLoading ? (
+                        <VStack className="flex-1 justify-center items-center" space="md">
+                            <ActivityIndicator size="large" color="#0891b2" />
+                            <Text size="sm" className="text-typography-400">Loading landmarks...</Text>
+                        </VStack>
+                    ) : (
+                        <FlatList
+                            data={landmarks}
+                            keyExtractor={(item) => item.id.toString()}
+                            showsVerticalScrollIndicator={false}
+                            ListEmptyComponent={
+                                <VStack className="items-center justify-center py-20" space="md">
+                                    <Icon as={MapIcon} size="xl" className="text-outline-200" />
+                                    <Text className="text-typography-400">No landmarks found</Text>
+                                </VStack>
+                            }
+                            renderItem={({ item }) => (
+                                <Pressable
+                                    className="mb-4 bg-background-0 border border-outline-100 rounded-3xl overflow-hidden active:bg-background-50"
+                                    onPress={() => router.navigate({
+                                        pathname: '/landmark/[id]/view',
+                                        params: {
+                                            id: item.id.toString(),
+                                            itineraryId: itineraryId.toString(),
+                                            currentCount: currentCount,
+                                        },
+                                    })}
+                                >
+                                    <HStack className="items-center p-3" space="md">
+                                        {/* Thumbnail Preview */}
+                                        <Box className="h-16 w-16 bg-background-100 rounded-2xl overflow-hidden">
+                                            {item.image_url ? (
+                                                <Image source={{ uri: item.image_url }} className="w-full h-full" />
+                                            ) : (
+                                                <Box className="items-center justify-center flex-1">
+                                                    <Icon as={MapPin} className="text-outline-300" />
+                                                </Box>
+                                            )}
+                                        </Box>
+
+                                        <VStack className="flex-1">
+                                            <Text className="font-bold text-typography-900" numberOfLines={1}>
+                                                {item.name}
+                                            </Text>
+                                            <HStack space="xs" className="items-center">
+                                                <Icon as={MapPin} size="xs" className="text-typography-400" />
+                                                <Text size="xs" className="text-typography-500">
+                                                    {item.municipality}
+                                                </Text>
+                                            </HStack>
+                                        </VStack>
+
+                                        <Button
+                                            size="sm"
+                                            action="primary"
+                                            className="rounded-xl h-10 w-10 p-0"
+                                            onPress={(e) => {
+                                                e.stopPropagation();
+                                                return addStopMutation.mutate(item.id);
+                                            }}
+                                            isDisabled={addStopMutation.isPending}
+                                        >
+                                            {addStopMutation.isPending && addStopMutation.variables === item.id ? (
+                                                <ButtonSpinner />
+                                            ) : (
+                                                <ButtonIcon as={Plus} />
+                                            )}
+                                        </Button>
+                                    </HStack>
+                                </Pressable>
+                            )}
+                        />
+                    )}
+                    <Button onPress={() => setShowManualDialog(true)}>
+                        <ButtonText>Add Manually </ButtonText>
+                    </Button>
+                </VStack>
+            </Box>
+        </>
     );
+}
+
+
+function LocationDialogSelection({
+    show, onClose,
+    onConfirmLocation,
+}: {
+    show: boolean, onClose: () => void,
+    onConfirmLocation: (location: GeoJSON.Position) => void
+}) {
+    const cameraRef = useRef<Mapbox.Camera>(null);
+    const [selectedLocation, setSelectedLocation] = useState<GeoJSON.Position | null>(null)
+
+    const handleConfirm = async () => {
+        if (selectedLocation) {
+            onConfirmLocation(selectedLocation)
+            onClose();
+        }
+    }
+
+    return (
+        <AlertDialog isOpen={show}
+            onClose={onClose}>
+            <AlertDialogBackdrop />
+            <AlertDialogContent>
+                <AlertDialogHeader>
+                    <VStack space="xs">
+                        <Heading size="lg">Add to Trip</Heading>
+                        <Text size="sm" className="text-typography-500">Select a location on the map to continue.</Text>
+                    </VStack>
+                </AlertDialogHeader>
+                <AlertDialogBody>
+                    <VStack className='py-6 px-2'>
+                        <CustomMapView
+                            cameraRef={cameraRef}
+                            mapViewProps={{
+                                style: { flex: 1, height: 400, },
+                                onPress(feature) {
+                                    const coordinates = (feature.geometry as GeoJSON.Point).coordinates;
+                                    setSelectedLocation(coordinates);
+                                },
+                            }}
+                        >
+                            {
+                                selectedLocation && (
+                                    <PointAnnotation
+                                        id='selected-location'
+                                        coordinate={selectedLocation}
+                                        draggable
+                                        onDragEnd={(feature) => {
+                                            setSelectedLocation(feature.geometry.coordinates);
+                                        }}
+                                    >
+                                        <View />
+                                    </PointAnnotation>
+                                )
+                            }
+                        </CustomMapView>
+                    </VStack>
+                </AlertDialogBody>
+                <AlertDialogFooter className='border-t border-outline-50 p-6 gap-3'>
+                    <HStack space='md'>
+                        <Button variant="outline" onPress={onClose}>
+                            <ButtonText>Cancel</ButtonText>
+                        </Button>
+                        <Button action="primary" onPress={handleConfirm}>
+                            <ButtonText>Confirm</ButtonText>
+                        </Button>
+                    </HStack>
+                </AlertDialogFooter>
+            </AlertDialogContent>
+        </AlertDialog>
+    )
 }
