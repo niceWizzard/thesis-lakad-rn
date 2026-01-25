55a171ee4ec9611bb978cb3c90c998c5ea506f28
niceWizzard <richard232manansala@gmail.com>
2026-01-25 15:26:15 +0800

    Made landmark images 32x32 and bg white

--- src/components/LandmarkMapView.tsx
+++ src/components/LandmarkMapView.tsx
@@ -180,13 +180,12 @@ const LandmarkMapView = ({
                             <Box
                                 style={{ elevation: 5 }}
 
-                                className={`p-1 bg-background-900 rounded-full shadow-lg 
-                                    ${selectedLandmark?.id === v.id ? 'border border-primary-500' : ''}`
-                                }
+                                className={`p-1 bg-white rounded-full shadow-lg
+                                    ${selectedLandmark?.id === v.id ? 'border border-primary-500' : ''}`}
                             >
                                 <Image
                                     source={{ uri: v.image_url || "https://via.placeholder.com/150" }}
-                                    style={{ width: 24, height: 24, borderRadius: 22 }}
+                                    style={{ width: 32, height: 32, borderRadius: 22 }}
                                 />
                             </Box>
 
@@ -199,8 +198,7 @@ const LandmarkMapView = ({
                                     transform: [{ rotate: '45deg' }],
                                     marginTop: -6,
                                     zIndex: -1
-                                }}
-                            />
+                                }} />
 
                             {/* Label with better padding */}
                             <Box className="mt-1 bg-background-100 px-2 py-0.5 rounded-full border border-outline-100 shadow-sm max-w-24">
