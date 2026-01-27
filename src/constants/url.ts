4dab910927a0b80a6c0606d63e59b40d0dda60f4
niceWizzard <richard232manansala@gmail.com>
2026-01-27 13:33:46 +0800

    r to get the final polyline

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,2 +1,6 @@
 export const MAPBOX_DIRECTIONS_URL = 'https://api.mapbox.com/directions/v5/mapbox';
 
+
+export const ORS_BASE_URL = `https://api.openrouteservice.org`
+export const ORS_DIRECTIONS_URL = `${ORS_BASE_URL}/v2/directions`
+
--- src/utils/distance/calculateItineraryDistance.ts
+++ src/utils/distance/calculateItineraryDistance.ts
@@ -1,31 +1,122 @@
-import { MAPBOX_DIRECTIONS_URL } from "@/src/constants/url";
-import { MapboxResponse } from "../navigation/fetchDirections";
+import { ACCESS_TOKEN as ORS_ACCESS_TOKEN } from "@/src/constants/token";
+import { ORS_DIRECTIONS_URL } from "@/src/constants/url";
 
-const MAX_WAYPOINTS = 25
+interface OrsError {
+    error: {
+        code: number,
+        message: string,
+    }
+}
 
-const ACCESS_TOKEN = process.env.EXPO_PUBLIC_MAPBOX_ACCESS_TOKEN ?? "";
+/**
+ * Main response structure for OpenRouteService V2 Direction/Routing
+ */
+export interface ORSRouteResponse {
+    bbox: [number, number, number, number];
+    routes: ORSRoute[];
+    metadata: ORSMetadata;
+}
+
+export interface ORSRoute {
+    summary: ORSSummary;
+    segments: ORSSegment[];
+    bbox: [number, number, number, number];
+    /** Encoded Polyline string */
+    geometry: string;
+    way_points: [number, number];
+    warnings?: ORSWarning[];
+    extras?: Record<string, any>;
+    departure?: string;
+    arrival?: string;
+}
+
+export interface ORSSummary {
+    distance: number;
+    duration: number;
+    ascent?: number;
+    descent?: number;
+}
+
+export interface ORSSegment {
+    distance: number;
+    duration: number;
+    steps: ORSStep[];
+    detourfactor?: number;
+    percentage?: number;
+    avgspeed?: number;
+    ascent?: number;
+    descent?: number;
+}
+
+export interface ORSStep {
+    distance: number;
+    duration: number;
+    /** Action code for symbolization (e.g., 0=Left, 1=Right) */
+    type: number;
+    instruction: string;
+    name: string;
+    way_points: [number, number];
+    maneuver: ORSManeuver;
+    exit_number?: number;
+    exit_bearings?: number[];
+}
+
+export interface ORSManeuver {
+    location: [number, number];
+    bearing_before: number;
+    bearing_after: number;
+}
+
+export interface ORSWarning {
+    code: number;
+    message: string;
+}
+
+export interface ORSMetadata {
+    id?: string;
+    attribution: string;
+    service: string;
+    timestamp: number;
+    query: Record<string, any>;
+    engine: ORSEngineInfo;
+}
+
+export interface ORSEngineInfo {
+    version: string;
+    build_date: string;
+    graph_date: string;
+    osm_date: string;
+    system_message?: string;
+}
+
+type ORSResponse<T> = T | OrsError
+
 
 
 export async function calculateItineraryDistance(waypoints: [number, number][]) {
-    let totalDistance = 0;
-    for (let i = 0; i < waypoints.length; i += MAX_WAYPOINTS) {
-        const chunk = waypoints.slice(i, i + MAX_WAYPOINTS)
-        const chunkedWaypoint = chunk.map(v => v.join(',')).join(';')
-        const url = new URL(`${MAPBOX_DIRECTIONS_URL}/driving/` + chunkedWaypoint);
-        url.searchParams.set('steps', 'false')
-        url.searchParams.set('alternatives', 'false')
-        url.searchParams.set('access_token', ACCESS_TOKEN)
+    try {
+        const response = await fetch(`${ORS_DIRECTIONS_URL}/driving-car`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': ORS_ACCESS_TOKEN,
+            },
+            body: JSON.stringify({
+                coordinates: waypoints,
+                radiuses: 1500,
+            })
+        })
+        const data: ORSResponse<ORSRouteResponse> = await response.json()
 
-        try {
-            const response = await fetch(url.toString())
-            const data: MapboxResponse = await response.json()
-            if (data.code !== "Ok")
-                throw new Error(`Mapbox API Error code: ${data.code}. ${JSON.stringify(data, null, 2)}`)
-            totalDistance += data.routes[0].distance || 0
-        } catch (error) {
-            console.error(error)
-            throw error
-        }
+        if ('error' in data)
+            throw new Error(`ORS Error:${JSON.stringify(data.error, null, 2)}`)
+
+        const route = data.routes[0];
+
+        return route.summary.distance;
+
+    } catch (error) {
+        console.error(error)
+        throw error
     }
-    return totalDistance;
 }
