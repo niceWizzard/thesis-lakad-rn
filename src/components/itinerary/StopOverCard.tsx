20be867a075d3f298625180660669c2a65ee236e
niceWizzard <richard232manansala@gmail.com>
2026-02-16 23:07:57 +0800

    fix: description overflowing in cards

--- src/components/itinerary/StopOverCard.tsx
+++ src/components/itinerary/StopOverCard.tsx
@@ -13,25 +13,32 @@ const StopOverCard = ({ stop }: {
 }) => {
     const landmark = stop.landmark
     return (
-        <View className='w-96 h-[400px] rounded-lg bg-background-0'>
+        <View className='w-96 h-[400px] rounded-lg bg-background-0 overflow-hidden'>
             {
                 landmark.image_url && (
                     <Image
                         source={{ uri: landmark.image_url }}
-                        className='w-full h-48 rounded-t-lg'
+                        className='w-full h-48'
                     />
                 )
             }
             <VStack
-                className='p-3 gap-3 '
+                className='p-4 gap-2 h-full'
             >
-                <Heading >{landmark.name}</Heading>
-                <HStack >
-                    <Badge>
+                <Heading size='md' numberOfLines={1}>{landmark.name}</Heading>
+
+                <HStack className='items-center gap-2'>
+                    <Badge size='sm' variant='solid' action='info'>
                         <BadgeText>{landmark.type}</BadgeText>
                     </Badge>
+                    <Text size='xs' className='text-typography-500'>
+                        {landmark.district}, {landmark.municipality}
+                    </Text>
                 </HStack>
-                <Text >{landmark.description}</Text>
+
+                <Text className='text-sm text-typography-600 mt-2' numberOfLines={5} ellipsizeMode='tail'>
+                    {landmark.description}
+                </Text>
             </VStack>
         </View>
     )
